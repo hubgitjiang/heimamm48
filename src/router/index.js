b615fda33b72cbd44d68f49a0636aa044df4893a
1122 <913009030@qq.com>
2020-03-15 15:32:14 +0800

    完成项目结构的搭建

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+// 导入 Vue
+import Vue from 'vue'
+// 导入 vue-router
+import VueRouter from 'vue-router'
+// 使用 vue-router
+Vue.use(VueRouter)
+// 创建一个新的 vue-router 对象
+var router = new VueRouter({
+    // 添加路由的规则
+    routes: [
+    ]
+})
+// 将新的对象暴露出去
+export default router
